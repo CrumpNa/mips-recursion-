7f6f886d339f43a97772df9e067e4b76fb7a3267
CrumpNa <nlcrump99@gmail.com>
2020-03-31 23:18:21 -0400

    commit 2

--- recursion.s
+++ recursion.s
@@ -1,5 +1,8 @@
-.data
 
+#My base: 36 
+
+.data
+userArr: .space 101 #allocating memory 
 
 prompt: .asciiz "\nEnter a string of 100 characters maximum."
 	
@@ -9,3 +12,24 @@ printanswer: .asciiz "\n "
 main:
 li $v0,4  #prints the original inputted string 
 la $a0, prompt 
+
+syscall
+
+li $v0, 8 #input from user 
+la $a0, userArr #loaded into array 
+
+li $t1,100
+syscall
+
+
+jal prgm1 
+sw $v1, output 
+
+lw $t2, userArr
+sub $sp, $sp, 12 #stack pointer initiated 
+
+
+sw 
+
+
+
